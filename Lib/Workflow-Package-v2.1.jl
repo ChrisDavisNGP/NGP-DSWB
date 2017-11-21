56da4bb9ffff4f9c14879b6d030815050e3fbe3b
ChrisDavisNGP <chris.davis@natgeo.com>
2017-11-21 17:30:58 -0500

    Centralize More Routines

--- Lib/Workflow-Package-v2.1.jl
+++ Lib/Workflow-Package-v2.1.jl
@@ -1,3 +1,12 @@
-function PageGroupDetailsWorkflow(TV::TimeVars,UP::UrlParams,SP::ShowParams,LV::LocalVars)
+function pageGroupDetailsWorkflow(TV::TimeVars,UP::UrlParams,SP::ShowParams,mobileView::ASCIIString,desktopView::ASCIIString)
 
+    pageGroupDetailsTables(UP.btView,mobileView,desktopView,UP.beaconTable,UP.pageGroup,TV.startTimeMsUTC,TV.endTimeMsUTC)
+    setTable(UP.btView);
+
+    localStatsDF = DataFrame()
+    statsDF = DataFrame()
+    medianThreshold = 0.0
+    statsDF = statsPGD()
+    medianThreshold = statsDF[1:1,:median][1]
+    
 end
