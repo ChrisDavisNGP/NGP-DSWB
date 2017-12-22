2b15e8ef973c341fb486c785974f643af6826471
ChrisDavisNGP <chris.davis@natgeo.com>
2017-12-22 08:13:09 -0500

    Notebook JL

--- Notebook-JL/Utilities/Beacons/Beacon-And-RT-Counts-v3.0.jl
+++ Notebook-JL/Utilities/Beacons/Beacon-And-RT-Counts-v3.0.jl
@@ -1,40 +1,52 @@
+using ODBC
+using DataFrames
 using DSWB
-setRedshiftEndpoint("dswb-natgeo")
-setTable("beacons_4744");
+using Formatting
+using URIParser
+using JSON
 
-bc = getBeaconCount();
-all = getBeaconsFirstAndLast();
-bcType = getBeaconCountByType();
+dsn = "dswb-natgeo" # Redshift esetTable(tableRt, tableType = "RESOURCE_TABLE")ndpoint
+table = "beacons_4744" # beacon table name
+tableRt = "beacons_4744_rt"
 
-methods(getBeaconCount)
+# Connect to Beacon Data
+setRedshiftEndpoint(dsn)
+setTable(table)
+setTable(tableRt, tableType = "RESOURCE_TABLE")
 
-beautifyDF(names!(bc[:,:],[symbol("Beacon Count")]))
-beautifyDF(all)
-beautifyDF(names!(bcType[:,:],[symbol("Beacon Type"),symbol("Beacon Count")]))
+include("../../../Lib/Include-Package-v2.1.jl")
 
-query("""\
-select * from beacons_4744 where page_group = 'News Article' limit 3
-""")
+TV = yesterdayTimeVariables(hours=1)
+UP = UrlParamsInit(scriptName)
+SP = ShowParamsInit()
+SP.debugLevel = 0   # Tests use even numbers with > tests, make this an odd number or zero
+SP.reportLevel = 10  # Set to 10 to see all non-debug info including long queries
+ShowParamsValidate(SP)
 
-query("""\
+if (SP.reportLevel > 9)
+    bc = getBeaconCount();
+    all = getBeaconsFirstAndLast();
+    #bcType = getBeaconCountByType();
+    beautifyDF(names!(bc[:,:],[symbol("Beacon Count")]))
+    beautifyDF(all)
+    #beautifyDF(names!(bcType[:,:],[symbol("Beacon Type"),symbol("Beacon Count")]))
+end
+
+t1DF = query("""\
 select * from beacons_4744 where page_group = 'News Article' and beacon_type = 'page view' limit 3
 """)
+standardChartTitle(TV,UP,SP,"News Article Page View Dump")
+beautifyDF(t1DF)
 
-query("""\
-select * from beacons_4744 where beacon_type = 'error' limit 3
-""")
-
-setTable("beacons_4744_rt", tableType = "RESOURCE_TABLE");
+t2DF = query("""select * from beacons_4744 where beacon_type = 'error' limit 3""")
+if (size(t2DF,1) > 0)
+    standardChartTitle(TV,UP,SP,"Error Beacon Dump")
+    beautifyDF(t2DF)
+end
 
 rtcnt = query("""select count(*) from beacons_4744_rt""");
-
-maxRt = query("""\
-select max("timestamp") from beacons_4744_rt
-""");
-
-minRt = query("""\
-select min("timestamp") from beacons_4744_rt
-""");
+maxRt = query("""select max("timestamp") from beacons_4744_rt""");
+minRt = query("""select min("timestamp") from beacons_4744_rt""");
 
 minStr = msToDateTime(minRt[1,:min]);
 maxStr = msToDateTime(maxRt[1,:max]);
@@ -44,6 +56,7 @@ printDf[:minStr] = minStr;
 printDf[:maxStr] = maxStr;
 ;
 
+standardChartTitle(TV,UP,SP,"Resource Information")
 beautifyDF(names!(rtcnt[:,:],[symbol("Resource Timing Count")]))
 beautifyDF(names!(printDf[:,:],[symbol("First RT"),symbol("Last RT")]))
 
