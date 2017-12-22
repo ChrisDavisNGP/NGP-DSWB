c48aeaed4517b828efdb9321c7eb48ab084aa2ba
ChrisDavisNGP <chris.davis@natgeo.com>
2017-12-21 19:51:26 -0500

    Notebook JL

---
+++ Notebook-JL/Rpt/Weekly-Top-100/Part-2-Mobile-iOS-News-Time-Top-100-v3.0.jl
@@ -0,0 +1,49 @@
+using ODBC
+using DataFrames
+using DSWB
+using Formatting
+using URIParser
+using JSON
+
+dsn = "dswb-natgeo" # Redshift esetTable(tableRt, tableType = "RESOURCE_TABLE")ndpoint
+table = "beacons_4744" # beacon table name
+tableRt = "beacons_4744_rt"
+
+# Connect to Beacon Data
+setRedshiftEndpoint(dsn)
+setTable(table)
+setTable(tableRt, tableType = "RESOURCE_TABLE")
+
+include("../../Lib/Include-Package-v2.1.jl")
+
+#TV = timeVariables(2017,5,9,16,0,2017,5,9,16,59)
+TV = prevWorkWeekTimeVariables()
+#TV = yesterdayTimeVariables()
+
+# This is the iOS Mobile Only, Time Based report
+
+UP = UrlParamsInit(scriptName)
+UP.agentOs = "iOS"
+UP.deviceType = "Mobile"
+UP.orderBy = "time"
+UP.pageGroup = "News Article"   #productPageGroup
+UP.sizeMin = 10000
+UP.timeLowerMs = 2000.0
+UP.timeUpperMs = 60000.0
+UP.urlRegEx = "%"   #localUrl
+UP.urlFull = "%"
+UP.usePageLoad=false
+UrlParamsValidate(UP)
+
+SP = ShowParamsInit()
+SP.devView=false
+SP.criticalPathOnly=true
+SP.debugLevel = 0   # Tests use even numbers with > tests, make this an odd number or zero
+ShowParamsValidate(SP)
+
+UP.limitRows = 125      # Consider the top 125 in the given category
+UP.samplesMin = 100     # Use only pages above 100 samples
+SP.showLines = 100      # The bigger this number the longer the script runs
+
+individualStreamlineWorkflow(TV,UP,SP)
+;
