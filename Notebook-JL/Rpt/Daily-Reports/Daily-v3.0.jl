2df5608effaf12d7a280291c4f630a5f9dc11a6b
ChrisDavisNGP <chris.davis@natgeo.com>
2017-12-23 01:18:06 -0500

    Notebook JL

---
+++ Notebook-JL/Rpt/Daily-Reports/Daily-v3.0.jl
@@ -0,0 +1,74 @@
+## Tables and Data Source setup
+
+using ODBC
+using DataFrames
+using DSWB
+using Formatting
+
+dsn = "dswb-natgeo" # Redshift endpoint
+table = "beacons_4744" # beacon table name
+tableRt = "beacons_4744_rt"
+
+# Connect to Beacon Data
+setRedshiftEndpoint(dsn)
+setTable(table)
+# setTable(tableRt, tableType = "RESOURCE_TABLE")
+;
+
+include("../../Lib/Include-Package-v2.1.jl")
+;
+
+#TV = timeVariables(2017,11,15,23,59,2017,11,16,23,59)
+#TV = weeklyTimeVariables(days=1)
+TV = yesterdayTimeVariables()
+;
+
+UP = UrlParamsInit("Daily")
+UP.agentOs = "%"
+UP.deviceType = "%"
+UP.limitRows = 250
+UP.pageGroup = "News Article"   #productPageGroup
+UP.samplesMin = 10
+UP.sizeMin = 10000
+UP.timeLowerMs = 2000.0
+UP.timeUpperMs = 60000.0
+UP.urlRegEx = "%"   #localUrl
+UP.urlFull = "%"
+UP.usePageLoad=false
+UrlParamsValidate(UP)
+
+SP = ShowParamsInit()
+SP.criticalPathOnly=true
+SP.devView=false
+SP.debugLevel = 0   # Tests use even numbers with > tests, make this an odd number or zero
+ShowParamsValidate(SP)
+;
+
+dailyWorkflow(TV,UP,SP)
+;
+
+# Desktop
+
+UP.deviceType = "Desktop"
+UrlParamsValidate(UP)
+
+dailyWorkflow(TV,UP,SP)
+;
+
+# Mobile Android OS
+
+UP.deviceType = "Mobile"
+UP.agentOs = "Android OS"
+UrlParamsValidate(UP)
+
+dailyWorkflow(TV,UP,SP)
+;
+
+# Mobile iOS
+
+UP.deviceType = "Mobile"
+UP.agentOs = "iOS"
+UrlParamsValidate(UP)
+
+dailyWorkflow(TV,UP,SP)
+;
---
+++ Notebook-JL/Rpt/Find-A-Page-View-Spike/Find-A-Page-View-Spike-v3.0.jl
@@ -0,0 +1,49 @@
+## Tables and Data Source setup
+
+using ODBC
+using DataFrames
+using DSWB
+using Formatting
+
+dsn = "dswb-natgeo" # Redshift endpoint
+table = "beacons_4744" # beacon table name
+tableRt = "beacons_4744_rt"
+
+# Connect to Beacon Data
+setRedshiftEndpoint(dsn)
+setTable(table)
+# setTable(tableRt, tableType = "RESOURCE_TABLE")
+;
+
+include("../../Lib/Include-Package-v2.1.jl")
+;
+
+#TV = timeVariables(2017,11,15,23,59,2017,11,16,23,59)
+#TV = weeklyTimeVariables(days=7)
+TV = yesterdayTimeVariables()
+;
+
+UP = UrlParamsInit("Find_A_Page_View_Spike")
+UP.agentOs = "%"
+UP.deviceType = "%"
+UP.limitRows = 250
+UP.pageGroup = "News Article"
+UP.samplesMin = 10
+UP.sizeMin = 10000
+UP.timeLowerMs = 2000.0
+UP.timeUpperMs = 60000.0
+UP.urlRegEx = "%"   #localUrl
+UP.urlFull = "%"
+UP.usePageLoad=false
+UrlParamsValidate(UP)
+
+SP = ShowParamsInit()
+SP.criticalPathOnly=true
+SP.devView=false
+SP.debugLevel = 0   # Tests use even numbers with > tests, make this an odd number or zero
+ShowParamsValidate(SP)
+
+;
+
+findAPageViewSpikeWorkflow(TV,UP,SP)
+;
