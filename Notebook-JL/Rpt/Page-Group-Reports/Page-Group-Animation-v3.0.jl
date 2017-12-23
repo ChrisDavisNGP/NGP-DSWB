3bb376756cbdcd600692e74548741d444dca5e22
ChrisDavisNGP <chris.davis@natgeo.com>
2017-12-22 19:08:32 -0500

    Notebook JL

---
+++ Notebook-JL/Rpt/Page-Group-Reports/Page-Group-Animation-v3.0.jl
@@ -0,0 +1,46 @@
+using ODBC
+using DataFrames
+using DSWB
+using Formatting
+
+dsn = "dswb-natgeo" # Redshift endpoint
+table = "beacons_4744" # beacon table name
+tableRt = "beacons_4744_rt"
+
+# Connect to Beacon Data
+setRedshiftEndpoint(dsn)
+setTable(table)
+
+# setTable(tableRt, tableType = "RESOURCE_TABLE")
+
+include("../../../Lib/Include-Package-v2.1.jl")
+
+customer = "Nat Geo"
+productPageGroup = "News Article" # primary page group
+localTable = "$(table)_productPage_view_prod"
+localTableRt = "$(tableRt)_productPage_view_prod"
+
+TV = timeVariables(2016,12,21,19,0,2016,12,21,23,59);
+
+# Create view to query only product page_group
+query("""create or replace view $localTable as (select * from $table where page_group = '$(productPageGroup)' and "timestamp" between $(TV.startTimeMsUTC) and $(TV.endTimeMsUTC))""")
+
+setTable(localTable)
+
+# Some routines use the unload events, some do not.  First count is all beacons such as page view and unload
+# where beacon_type = 'page view'
+#query("""SELECT count(*) FROM $localTable""")
+
+retailer_results = getLatestResults(hours=1, minutes=30, table_name="$(localTable)")
+size(retailer_results)
+
+# drop some of the fields to make the output easier to read
+
+#delete!(retailer_results,[:geo_rg,:geo_city,:geo_org,:user_agent_major,:user_agent_osversion,:user_agent_os,:user_agent_model,:referrer])
+delete!(retailer_results,[:geo_rg,:geo_city,:geo_org,:user_agent_major,:user_agent_osversion,:user_agent_os,:user_agent_model])
+
+doit(retailer_results, showDimensionViz=true, showProgress=true);
+
+q = query(""" drop view if exists $localTable;""")
+q = query(""" drop view if exists $localTableRt;""")
+;
---
+++ Notebook-JL/Rpt/Page-Group-Reports/Page-Group-Details-v3.0.jl
@@ -0,0 +1,46 @@
+using ODBC
+using DataFrames
+using DSWB
+using Formatting
+
+dsn = "dswb-natgeo" # Redshift endpoint
+table = "beacons_4744" # beacon table name
+tableRt = "beacons_4744_rt"
+
+# Connect to Beacon Data
+setRedshiftEndpoint(dsn)
+setTable(table)
+# setTable(tableRt, tableType = "RESOURCE_TABLE")
+
+include("../../../Lib/Include-Package-v2.1.jl")
+
+#TV = timeVariables(2017,10,27,23,59,2017,11,3,23,59)
+#TV = weeklyTimeVariables(days=7)
+TV = yesterdayTimeVariables()
+
+UP = UrlParamsInit("Page_Group_Details")
+UP.agentOs = "%"
+UP.deviceType = "Mobile"
+UP.limitRows = 10
+#UP.limitRows = 250
+UP.pageGroup = "Your Shot"   #productPageGroup
+UP.samplesMin = 10
+UP.sizeMin = 10000
+UP.timeLowerMs = 2000.0
+UP.timeUpperMs = 60000.0
+UP.urlRegEx = "%"   #localUrl
+UP.urlFull = "%"
+UP.usePageLoad=false
+UrlParamsValidate(UP)
+
+SP = ShowParamsInit()
+SP.devView=false
+SP.criticalPathOnly=true
+SP.debugLevel = 0   # Tests use even numbers with > tests, make this an odd number or zero
+ShowParamsValidate(SP)
+
+mobileView = "$(table)_$(scriptName)_Mobile_view_prod"
+desktopView = "$(table)_$(scriptName)_Desktop_view_prod"
+
+pageGroupDetailsWorkflow(TV,UP,SP,mobileView,desktopView)
+;
