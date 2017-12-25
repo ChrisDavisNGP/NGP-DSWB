062ee85e409149a732f0c35261eb051cf38457cb
ChrisDavisNGP <chris.davis@natgeo.com>
2017-12-25 06:13:28 -0500

    Convert

--- Notebook-JL/Rpt/Large-Pages/Show-Requests-For-Large-Pages-v3.0.jl
+++ Notebook-JL/Rpt/Large-Pages/Show-Requests-For-Large-Pages-v3.0.jl
@@ -11,38 +11,17 @@ tableRt = "beacons_4744_rt"
 setRedshiftEndpoint(dsn)
 setTable(table)
 setTable(tableRt, tableType = "RESOURCE_TABLE")
-;
 
 include("../../../Lib/Include-Package-v2.1.jl")
-;
 
+TV = pickTime()
 #TV = timeVariables(2017,6,8,10,59,2017,6,8,12,59)
-#TV = weeklyTimeVariables(days=1)
-TV = yesterdayTimeVariables()
-;
 
 UP = UrlParamsInit(scriptName)
-UP.agentOs = "%"
-UP.deviceType = "Mobile"
-UP.limitRows = 250
-UP.pageGroup = "News Article"   #productPageGroup
-UP.samplesMin = 10
-UP.sizeMin = 10000
-UP.timeLowerMs = 2000.0
-UP.timeUpperMs = 600000.0
-UP.urlRegEx = "%"   #localUrl
-UP.urlFull = "%"
-UP.usePageLoad=false
 UrlParamsValidate(UP)
 
 SP = ShowParamsInit()
-SP.criticalPathOnly=true
-SP.devView=false
-SP.debugLevel = 10   # Tests use even numbers with > tests, make this an odd number or zero
-SP.showLines = 25
 ShowParamsValidate(SP)
-
-;
 
 showRequestsForLargePagesWorkflow(TV,UP,SP)
 ;
