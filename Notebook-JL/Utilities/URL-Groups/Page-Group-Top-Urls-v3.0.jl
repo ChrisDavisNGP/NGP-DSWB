32f0f2e14c44a952821c4b8fca4ae734e1a4da5e
ChrisDavisNGP <chris.davis@natgeo.com>
2017-12-26 21:53:16 -0500

    Convert

--- Notebook-JL/Utilities/URL-Groups/Page-Group-Top-Urls-v3.0.jl
+++ Notebook-JL/Utilities/URL-Groups/Page-Group-Top-Urls-v3.0.jl
@@ -14,29 +14,13 @@ setTable(table)
 
 include("../../../Lib/Include-Package-v2.1.jl")
 
+TV = pickTime()
 #TV = timeVariables(2017,6,8,10,59,2017,6,8,12,59)
-#TV = weeklyTimeVariables(days=7)
-TV = yesterdayTimeVariables()
 
 UP = UrlParamsInit(scriptName)
-UP.agentOs = "%"
-UP.deviceType = "%"
-UP.limitRows = 250
-UP.pageGroup = "Unknown"   #productPageGroup
-UP.samplesMin = 10
-UP.sizeMin = 10000
-UP.timeLowerMs = 2000.0
-UP.timeUpperMs = 60000.0
-UP.urlRegEx = "%"   #localUrl
-UP.urlFull = "%"
-UP.usePageLoad=false
 UrlParamsValidate(UP)
 
 SP = ShowParamsInit()
-SP.criticalPathOnly=true
-SP.devView=false
-SP.debugLevel = 0   # Tests use even numbers with > tests, make this an odd number or zero
-SP.showLines = 25
 ShowParamsValidate(SP)
 
 dumpDataFieldsV2Workflow(TV,UP,SP)
