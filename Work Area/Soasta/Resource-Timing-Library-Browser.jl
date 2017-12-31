bbe38896cd6cd988c02b08d594c4e02fdbe00dd1
ChrisDavisNGP <chris.davis@natgeo.com>
2017-12-31 12:04:32 -0500

    Convert

--- Work Area/Soasta/Resource-Timing-Library-Browser.jl
+++ Work Area/Soasta/Resource-Timing-Library-Browser.jl
@@ -1,59 +1,65 @@
 using RT;
+using ODBC
+using DataFrames
+using DSWB
+using Formatting
 
-dsn = "vfdsn";
-beacons = "anonymous_beacons";
-resources = "$(beacons)_rt";
+dsn = "dswb-natgeo" # Redshift endpoint
+table = "beacons_4744" # beacon table name
+tableRt = "beacons_4744_rt"
+sessions = "beacons_4744_sessions";
 
-browser = "Chrome Mobile";
-datepart = :hour;
-worstResource = "http://www.site.com/assets/css/fonts/fontawesome-webfont.woff?v=4.5.0";
-
-sessions = "$(beacons)_sessions";
-resourceLT = "$(resources)_lt";
-
-setRedshiftEndpoint(dsn);
-setTable(beacons);
-setTable(resources; tableType = RESOURCE_TABLE);
+# Connect to Beacon Data
+setRedshiftEndpoint(dsn)
+setTable(table)
+setTable(tableRt, tableType = "RESOURCE_TABLE")
 setTable(sessions; tableType = SESSIONS_TABLE);
 
-getBeaconsFirstAndLast()
+include("../../../Lib/Include-Package.jl")
 
-startTime = getBeaconsFirstAndLast()[symbol("First Beacon")][1];
-endTime = getBeaconsFirstAndLast()[symbol("Last Beacon")][1];
-topPageGroup = "Product";
+TV = pickTime()
+#TV = timeVariables(2017,11,15,23,59,2017,11,16,23,59)
+
+UP = UrlParamsInit(scriptName)
+UrlParamsValidate(UP)
+
+SP = ShowParamsInit()
+ShowParamsValidate(SP)
+
+browser = "Chrome Mobile";
+worstResource = "http://www.site.com/assets/css/fonts/fontawesome-webfont.woff?v=4.5.0";
 conversionMetric = :custom_metrics_1;
-
 setConversionMetric(conversionMetric);
 
-domain = getDomains(startTime, endTime)[1]
+domain = getDomains(TV.startTimeUTC, TV.endTimeUTC)[1]
 
 # Resource Analysis
----
-Analysis using the `RT` library for the resources used on `www.site.com`.
+#---
+#Analysis using the `RT` library for the resources used on `www.site.com`.
 
-chartConversionsByBrowser(startTime, endTime);
+chartConversionsByBrowser(TV.startTimeUTC, TV.endTimeUTC);
 
 # For official documentation, try using: ?chartResourceStats
 # The value passed by domain indicates what is considered a first party.  All non-matching resources will be considered 3rd party.
 # The dimension argument indicate how you want the resources to be grouped.  Possible arguments: RESOURCE_TYPE (default; img, css, etc.), PG (page group), OS (Windows, Mac OS X, etc.),
 #  Browser (Chrome, IE, Firefox, etc.), DEVICE_TYPE (Desktop, Mobile, etc.), COUNTRY_CODE (United States, Germany, UK, etc.), and beacon columns are also usable.
-chartResourceStats(domain, startTime, endTime; dimension = BROWSER);
+chartResourceStats(domain, TV.startTimeUTC, TV.endTimeUTC; dimension = BROWSER);
 
-(**Note**: `Mobile Safari` and `Safari` do not show up in the resource analysis because `resource timing` is _not_ supported by those browsers.  Resources that appear under `Mobile Safari` and `Safari` should be ignored.)
+#(**Note**: `Mobile Safari` and `Safari` do not show up in the resource analysis because `resource timing` is _not_ supported by those browsers.  Resources that appear under `Mobile Safari` and `Safari` should be ignored.)
 
 # Resource Load Times by Resource Type
----
-The following graphs use box-plots to compare the different groups of data.  If you're not familiar with box-plots, watch this <a href="https://www.youtube.com/watch?v=b2C9I8HuCe4&feature=youtu.be&t=16">short video</a> to understand how they work.
+#---
+#The following graphs use box-plots to compare the different groups of data.  If you're not familiar with box-plots, watch this <a href="https://www.youtube.com/watch?v=b2C9I8HuCe4&feature=youtu.be&t=16">short video</a> to understand how they work.
 
 # For official documentation, try using: ?chartLoadTimeStats
 # The dimension argument indicate how you want the load-times to be grouped.  Possible arguments: RESOURCE_TYPE (default; img, css, etc.), PG (page group), OS (Windows, Mac OS X, etc.),
 #  Browser (Chrome, IE, Firefox, etc.), DEVICE_TYPE (Desktop, Mobile, etc.), COUNTRY_CODE (United States, Germany, UK, etc.), and beacon columns are also usable.
 # The url indicates the first-party resource domain to identify the resources against.
 # Setting the isFirstParty to false indicates you want to look at all resources that do not come from the domain provided in the url argument.
-chartLoadTimeStats(startTime, endTime; dimension = BROWSER, url = domain, isFirstParty = false);
+chartLoadTimeStats(TV.startTimeUTC, TV.endTimeUTC; dimension = BROWSER, url = domain, isFirstParty = false);
 
 # Top 10 Worst Resources on Chrome Mobile (with at least 5,000 requests)
----
+#---
 
 # For official documentation, try using: ?chartResources
 # Only the resources coming from the specified browser will be considered.
@@ -64,17 +70,17 @@ chartLoadTimeStats(startTime, endTime; dimension = BROWSER, url = domain, isFirs
 #  will short-circuit the function call with a prepared result so remove it if you want to run the DB query to get new results.
 file = "browserResources";
 #chartResources(startTime, endTime; browser = browser, minResourceCount = 5_000, inputFile = file);
-chartResources(startTime, endTime; browser = browser, minResourceCount = 5_000, outputFile = file);
+chartResources(TV.startTimeUTC, TV.endTimeUTC; browser = browser, minResourceCount = 5000, outputFile = file);
 
 # Trending Resource For Resource Load Time Insight
----
+#---
 
 # For official documentation, try using: ?chartResourceTrend
 # Only the resources coming from the specified browser will be considered.
 # This showTable argument allow you to hide the table and only show the chart, if it does not add to the presentation.
 file = "resourceTrending";
 #chartResourceTrend(worstResource, startTime, endTime, datepart; browser = browser, inputFile = file);
-chartResourceTrend(worstResource, startTime, endTime, datepart; browser = browser, outputFile = file);
+chartResourceTrend(worstResource, TV.startTimeUTC, TV.endTimeUTC, TV.datepart; browser = browser, outputFile = file);
 
 The graph below is the same graph (as the one above) but showing the median, 75th and 95th percentiles.
 
@@ -82,4 +88,4 @@ The graph below is the same graph (as the one above) but showing the median, 75t
 # Only the resources coming from the specified browser will be considered.
 # This showTable argument allow you to hide the table and only show the chart, if it does not add to the presentation.
 # Percentiles argument allow you to enter the percentiles of load times you want to graph so you have a better idea of the load time spread over time.
-chartResourceTrend(worstResource, startTime, endTime, datepart; browser = browser, percentiles = AbstractFloat[0.5, 0.75, 0.9])
+chartResourceTrend(worstResource, TV.startTimeUTC, TV.endTimeUTC, TV.datepart; browser = browser, percentiles = AbstractFloat[0.5, 0.75, 0.9])
