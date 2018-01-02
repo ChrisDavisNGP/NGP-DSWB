0c2c23afba85aeb9968516822cbed05ceddd2c94
Chris Davis <chris.davis@natgeo.com>
2018-01-02 08:18:06 -0500

    Improve

--- Lib/Structures-Package.jl
+++ Lib/Structures-Package.jl
@@ -134,6 +134,7 @@ function UrlParamsValidate(UP::UrlParams)
         UP.pageGroup != "No Page Group" &&
         UP.pageGroup != "Unknown" &&
         UP.pageGroup != "Adventure AEM" &&
+        UP.pageGroup != "Dev-QA-Stage-UAT" &&
         UP.pageGroup != "Environment AEM"
         )
         println("Warning: pageGroup is an unusual value \"",UP.pageGroup,"\", See mPulse All Page Group filter for all values.")
