d79d791d3a3e5e08a47c8b1af730fbbfd0e70f49
Chris Davis <chris.davis@natgeo.com>
2018-01-10 09:36:26 -0500

    Bug Fix

--- Lib/Structures-Package.jl
+++ Lib/Structures-Package.jl
@@ -286,7 +286,7 @@ function CurlParamsInit(nb::ASCIIString)
     end
 
     if isdefined(:CuSyntheticMonitor)
-        CU.SyntheticMonitor = CuSyntheticMonitor
+        CU.syntheticMonitor = CuSyntheticMonitor
     end
 
     if isdefined(:CuOldStart)
