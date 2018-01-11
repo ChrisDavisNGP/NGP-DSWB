44de7fa5ec14d356c226705a5fa76e50ee0a0563
ChrisDavisNGP <chris.davis@natgeo.com>
2018-01-11 10:23:26 -0500

    Daily

--- Lib/New-Relic-Package.jl
+++ Lib/New-Relic-Package.jl
@@ -79,7 +79,7 @@ function curlSelectDurationAndSize(SP::ShowParams,CU::CurlParams,startTimeNR::AS
     curlCommand = "https://insights-api.newrelic.com/v1/accounts/78783/query?nrql=" *
         "SELECT%20stddev(totalResponseBodySize)%2Caverage(totalResponseBodySize)%2Cstddev(duration)%2Caverage(duration)%20" *
         "FROM%20SyntheticCheck%20facet%20monitorName%20since%20%27" * startTimeNR * "%27%20until%20%27" * endTimeNR *
-        "%27%20with%20TIMEZONE%20%27America%2FNew_York%27%20limit%20500%20COMPARE%WITH%20" * compareWith
+        "%27%20with%20TIMEZONE%20%27America%2FNew_York%27%20limit%20500%20COMPARE%20WITH%20" * compareWith
     curlStr = ["-H","$apiKey","$curlCommand"]
 
     #SELECT stddev(totalResponseBodySize),average(totalResponseBodySize),stddev(duration),average(duration) FROM SyntheticCheck facet monitorName since '2018-01-10 00:07:00' until '2018-01-10 17:00:00' with TIMEZONE 'America/New_York' limit 500
