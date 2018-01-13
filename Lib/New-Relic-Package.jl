5d38de3d62d34f812ff4b69025c0424e5999df8a
ChrisDavisNGP <chris.davis@natgeo.com>
2018-01-13 14:20:12 -0500

    Compare

--- Lib/New-Relic-Package.jl
+++ Lib/New-Relic-Package.jl
@@ -484,6 +484,7 @@ function dumpHostGroups(SP::ShowParams,NR::NrParams;showGroups::Bool=true)
     if SP.debugLevel > 8
         println("Starting dumpHostGroups")
     end
+    WellKnownHostDirectory = wellKnownHostEncyclopedia(SP.debug);
 
     hostGroupsDF = DataFrame(host=ASCIIString[],bodySize=Int64[],resources=Int64[],duration=Float64[])
 
