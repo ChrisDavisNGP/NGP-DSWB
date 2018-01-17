6db9bc24410f03e7e960865945433be2f801753f
Chris Davis <chris.davis@natgeo.com>
2018-01-17 15:54:32 -0500

    Stats

--- Lib/New-Relic-Package.jl
+++ Lib/New-Relic-Package.jl
@@ -320,10 +320,10 @@ function quickTimestampViz(NR::NrParams,theSymbol::Symbol,Title::ASCIIString)
         drawDF[:col1] = NR.results.row[:timestamp]
         drawDF[:data1] = NR.results.row[theSymbol]
 
-        dv = Float64[]
-        dv = NR.results.row[theSymbol] * 1.0
-        dv = dropna(dv)
-        println(Title," stddev=",std(dv)," skew=",skewness(dv))
+        #dv = Float64[]
+        #dv = NR.results.row[theSymbol] * 1.0
+        #dv = dropna(dv)
+        #println(Title," stddev=",std(dv)," skew=",skewness(dv))
 
         c3 = drawC3Viz(drawDF; axisLabels=["Seconds"],dataNames=[Title], mPulseWidget=false, chart_title= Title * " Chart", vizTypes=["line"])
     catch y
