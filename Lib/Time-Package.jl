1376fc0a27ea2325221114eb7ee5e26b6e3b5088
Chris Davis <chris.davis@natgeo.com>
2018-01-19 10:44:01 -0500

    OnPageLoad

--- Lib/Time-Package.jl
+++ Lib/Time-Package.jl
@@ -63,8 +63,8 @@ function timeVariables(
         tempTime.endTime = DateTime(Y2,M2,D2,H2,MM2)
         tempTime.startTimeMs = datetimeToMs(tempTime.startTime)
         tempTime.endTimeMs = datetimeToMs(tempTime.endTime)
-        tempTime.startTimeStr = "$(padDateTime(tempTime.startTime))"
-        tempTime.endTimeStr = "$(padDateTime(tempTime.endTime))"
+        tempTime.startTimeStr = "$(padDateTime(DateTime(tempTime.startTime,"YYYY-mm-dd HH:MM:SS")))"
+        tempTime.endTimeStr = "$(padDateTime(DateTime(tempTime.endTime,"YYYY-mm-dd HH:MM:SS")))"
 
         tempTime.startTimeUTC = datetimeToUTC(tempTime.startTime, TimeZone("America/New_York"))
         tempTime.endTimeUTC = datetimeToUTC(tempTime.endTime, TimeZone("America/New_York"))
